fb258d309501bba9e65d7f57e614b1e39eb272dc
fire-eggs <lifeattickville@gmail.com>
2026-04-18 16:27:50 -0400

    populating table rows with selected tags, buttons

--- src/static/edittag.js
+++ src/static/edittag.js
@@ -14,6 +14,11 @@ function performEditTag() {
     mgr.classList.add('active');
     edit_tag_input.value = '';
     edit_tag_suggestions.innerHTML = '';
+    edit_tag_array.length = 0;
+    selected_edit_tags_div.innerHTML = '';
+
+    const tbody = document.getElementById("editTableBody");
+    tbody.innerHTML = "";
 
 }
 
@@ -39,4 +44,53 @@ function searchTagInput() {
 
 function renderEditTags() {
     renderTags(selected_edit_tags_div, edit_tag_array, 'general', 'edit_tags_magic');
+    updateTable();
+}
+
+function updateTable() {
+    const tbody = document.getElementById("editTableBody");
+    tbody.innerHTML = "";
+    
+    edit_tag_array.forEach( tag => {
+        const newrow = tbody.insertRow(-1);
+        
+        const cell0 = newrow.insertCell(0);
+        const cell1 = newrow.insertCell(1);
+        const cell2 = newrow.insertCell(2);
+        
+        cell0.textContent = tag.tag_id;
+        cell1.textContent = tag.tag_name;
+        cell2.textContent = tag.class_name;
+
+        var btnEdit = makeButton("Edit", "44CCEB", `onTagEdit(${tag.tag_id})`);
+        newrow.appendChild(btnEdit);
+        
+        var btnSave = makeButton("Save", "2DBF64", `onTagSave(${tag.tag_id})`);
+        newrow.appendChild(btnSave);
+
+        var btnDel = makeButton("Delete", "ED5650", `onTagDelete(${tag.tag_id})`);
+        newrow.appendChild(btnDel);
+        
+    });
+}
+
+function makeButton(text, color, click) {
+    var td = document.createElement("td");
+    var btn = document.createElement("input");
+    btn.setAttribute('type', 'button');
+    btn.setAttribute('value', text);
+    btn.setAttribute('style', `background-color:#${color};`);
+    btn.setAttribute('onclick', click);
+    td.appendChild(btn);
+    return td;
+}
+
+function onTagEdit(tag_id) {
+    alert(`Edit: ${tag_id}`);
+}
+function onTagSave(tag_id) {
+    alert(`Save: ${tag_id}`);
+}
+function onTagDelete(tag_id) {
+    alert(`Del: ${tag_id}`);
 }
