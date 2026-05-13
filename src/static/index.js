fbd62cff8af29a1a9b9c2b4e590b3f90ce3ac43f
fire-eggs <lifeattickville@gmail.com>
2026-05-13 14:02:29 -0400

    Quick & dirty fix: tag class string vs int compare

--- src/static/index.js
+++ src/static/index.js
@@ -151,10 +151,10 @@ function selectAll() {
 function generateTagPill(text, tag_id, tagtype, letter="x") {
     let tagclass = "general";
     // TODO consider having the db return the tagclass string, not the number
-    if (tagtype === 4) { tagclass = "character"; }
-    if (tagtype === 12) { tagclass = "artist"; }
-    if (tagtype === 14) { tagclass = "franchise"; }
-    if (tagtype === 99 || tagtype === 32) { tagclass = "newtext"; }  // special: user has typed new tag not in database
+    if (tagtype === 4 || tagtype === "4") { tagclass = "character"; }
+    if (tagtype === 12 || tagtype === "12") { tagclass = "artist"; }
+    if (tagtype === 14 || tagtype === "14") { tagclass = "franchise"; }
+    if (tagtype === 99 || tagtype === 32 || tagtype === "32") { tagclass = "newtext"; }  // special: user has typed new tag not in database
     return `<span class="pill ${tagclass}">${text} <button data-id=${tag_id} data-tagname="${text}" data-typeid=${tagtype}>${letter}</button></span>`;
 }
 
